edb4f22f573ef334ff9459f8d4b9883d4ae0fa3c
Mint <audwl03071@gmail.com>
2025-06-24 11:32:26 +0900

    merge: previous mission1

---
+++ src/test/java/roomescape/MissionStepTest.java
@@ -0,0 +1,24 @@
+package roomescape;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.annotation.DirtiesContext;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class MissionStepTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Test
+    void 일단계() {
+        RestAssured.given().log().all()
+                .when().get("/admin")
+                .then().log().all()
+                .statusCode(200);
+    }
+}
