402a4d63205059e98d7004ae01c7d588ffe4abe4
Mint <audwl03071@gmail.com>
2025-06-24 13:34:59 +0900

    test: e2e 테스트 클래스명에 e2e 키워드 추가

--- src/test/java/roomescape/e2e/ReservationTimeE2ETest.java
+++ src/test/java/roomescape/e2e/ReservationTimeE2ETest.java
@@ -4,18 +4,18 @@ import static org.hamcrest.Matchers.is;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.annotation.DirtiesContext;
-import roomescape.dto.request.ReservationPreservationRequest;
-import roomescape.e2e.fixture.TestFixture;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
-public class ReservationTest {
+public class ReservationTimeE2ETest {
 
     @LocalServerPort
     int port;
@@ -26,47 +26,38 @@ public class ReservationTest {
     }
 
     @Test
-    void findReservations() {
-        TestFixture.saveReservation();
-        RestAssured.given().log().all()
-                .when().get("/reservations")
-                .then().log().all()
-                .statusCode(200)
-                .body("size()", is(1));
-    }
-
-    @Test
-    void saveReservation() {
-        Long timeId = TestFixture.saveReservationTime();
+    void saveReservationTime() {
+        Map<String, String> params = new HashMap<>();
+        params.put("startAt", "10:00");
 
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
-                .body(new ReservationPreservationRequest("mint", "2026-02-05", timeId))
-                .when().post("/reservations")
+                .body(params)
+                .when().post("/times")
                 .then().log().all()
-                .statusCode(201)
-                .body("id", is(1));
+                .statusCode(201);
 
         RestAssured.given().log().all()
-                .when().get("/reservations")
+                .when().get("/times")
                 .then().log().all()
                 .statusCode(200)
                 .body("size()", is(1));
     }
 
     @Test
-    void deleteReservation() {
-        saveReservation();
+    void deleteReservationTime() {
+        saveReservationTime();
 
         RestAssured.given().log().all()
-                .when().delete("/reservations/1")
+                .when().delete("/times/1")
                 .then().log().all()
                 .statusCode(204);
 
         RestAssured.given().log().all()
-                .when().get("/reservations")
+                .when().get("/times")
                 .then().log().all()
                 .statusCode(200)
                 .body("size()", is(0));
     }
+
 }
